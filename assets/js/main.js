27ef014cc56c75b2af93a40e680cbbbe33aafc5c
bondopangaji <60285086+bondopangaji@users.noreply.github.com>
2022-05-15 11:37:38 +0700

    + navbar, hero, featured, testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,106 @@
-// TODO: create scripts
+/**
+ * Main function
+ */
+(function () {
+    "use strict";
+
+    /* ------------------
+    * Helper functions  *
+    * Ref: GetBootstrap *
+    ------------------- */
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+     * Easy on scroll event listener 
+    */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+
+    /* ----------------------
+    * Mobile navigation bar *
+    ----------------------- */
+
+    /**
+     * Nav toggle
+     */
+    on('click', '.mobile-nav-toggle', function (e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('ri-menu-3-fill')
+        this.classList.toggle('ri-close-fill')
+    })
+
+    /**
+     * Nav dropdowns activate
+     */
+    on('click', '.navbar .dropdown > a', function (e) {
+        if (select('#navbar').classList.contains('navbar-mobile')) {
+            e.preventDefault()
+            this.nextElementSibling.classList.toggle('dropdown-active')
+        }
+    }, true)
+
+
+    /**
+     * Add .header-scrolled class to #header when the page is scrolled
+     */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+
+    /* ------------------
+    * Animate on scroll *
+    --------------------*/
+
+    /**
+     * Initialize AOS
+     */
+    function aos_init() {
+        AOS.init({
+            duration: 1000,
+            easing: "ease-in-out",
+            once: true,
+            mirror: false
+        });
+    }
+    window.addEventListener('load', () => {
+        aos_init();
+    });
+
+})();
