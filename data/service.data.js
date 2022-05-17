363ccc1f0edc1abe5bf26809419d5a743813b10d
Minan12 <85333801+Minan12@users.noreply.github.com>
2022-05-18 01:39:01 +0700

    +load service data w/alpine

---
+++ data/service.data.js
@@ -0,0 +1,82 @@
+/* -----------------------------------
+ * Helper Function to Load JSON Data *
+ ----------------------------------- */
+
+function loadData(){
+    return serviceData;
+}
+
+/* -----------
+ * JSON Data *
+ ----------- */
+const serviceData = {
+    service: {
+        service_cards: [
+            {
+                title: "Mini Groom",
+                descriptions: `<p>Bath</p>
+                <p>Blow Dry</p>
+                <p>Facial & Feet Trimming</p>
+                <p>Genital Area Trimming</p>
+                <p>Ear Cleaning</p>
+                <p>Nail Cleaning</p>
+                <p class="text-decoration-line-through opacity-25">Brush Out</p>
+                <p class="text-decoration-line-through opacity-25">Anal Gland Expression</p>
+                <p class="text-decoration-line-through opacity-25">Scissoring</p>
+                <p class="text-decoration-line-through opacity-25">Hair Styling</p>
+                <p class="text-decoration-line-through opacity-25">Clipper Cut (Full Body)</p>`,
+                btn_link: "Service.html",
+                btn_text: "Buy Now",
+            },
+            {
+                title: "Basic Groom",
+                descriptions: `<p>Bath</p>
+                <p>Blow Dry</p>
+                <p>Facial & Feet Trimming</p>
+                <p>Genital Area Trimming</p>
+                <p>Ear Cleaning</p>
+                <p>Nail Cleaning</p>
+                <p>Brush Out</p>
+                <p>Anal Gland Expression</p>
+                <p class="text-decoration-line-through opacity-25">Scissoring</p>
+                <p class="text-decoration-line-through opacity-25">Hair Styling</p>
+                <p class="text-decoration-line-through opacity-25">Clipper Cut (Full Body)</p>`,
+                btn_link: "Service.html",
+                btn_text: "Buy Now",
+            },
+            {
+                title: "Premium Groom",
+                descriptions: `<p>Bath</p>
+                <p>Blow Dry</p>
+                <p>Facial & Feet Trimming</p>
+                <p>Genital Area Trimming</p>
+                <p>Ear Cleaning</p>
+                <p>Nail Cleaning</p>
+                <p>Brush Out</p>
+                <p>Anal Gland Expression</p>
+                <p>Scissoring</p>
+                <p>Hair Styling</p>
+                <p class="text-decoration-line-through opacity-25">Clipper Cut (Full Body)</p>`,
+                btn_link: "Service.html",
+                btn_text: "Buy Now",
+            },
+            {
+                title: "Deluxe Groom",
+                descriptions: `<p>Bath</p>
+                <p>Blow Dry</p>
+                <p>Facial & Feet Trimming</p>
+                <p>Genital Area Trimming</p>
+                <p>Ear Cleaning</p>
+                <p>Nail Cleaning</p>
+                <p>Brush Out</p>
+                <p>Anal Gland Expression</p>
+                <p>Scissoring</p>
+                <p>Hair Styling</p>
+                <p>Clipper Cut (Full Body)</p>`,
+                btn_link: "Service.html",
+                btn_text: "Buy Now",
+            },
+        ],
+        description: "Prices are dependent on breed, coat condition, and cat temperament.",
+    },
+};
