87f21c60adfaa7f6fb7c3cf3632cfed0570e4f73
Minan12 <85333801+Minan12@users.noreply.github.com>
2022-05-18 11:32:22 +0700

    +load contact data w/alpine

---
+++ data/contact.data.js
@@ -0,0 +1,16 @@
+/* -----------------------------------
+ * Helper Function to Load JSON Data *
+ ----------------------------------- */
+
+function loadData() {
+    return contactData;
+}
+
+/* -----------
+* JSON Data *
+----------- */
+const contactData = {
+    contact: {
+        business_loc_map_link: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3954.4836276587166!2d109.2511412!3d-7.6310231!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e654128eaaaaaab%3A0x3e90bcf5beb75031!2sStasiun%20Kroya!5e0!3m2!1sen!2sid!4v1652691755142!5m2!1sen!2sid",
+    },
+};
